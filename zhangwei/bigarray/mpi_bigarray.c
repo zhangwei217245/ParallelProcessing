360be1af4638281643c76d371f0f878a3f4efedf
Wei Zhang <x-spirit.zhang@ttu.edu>
2015-09-07 00:25:02 -0500

    Bug-free code finished. output is directed to files under ouput folder. gitigonre is changed

--- zhangwei/bigarray/mpi_bigarray.c
+++ zhangwei/bigarray/mpi_bigarray.c
@@ -21,7 +21,7 @@ int main (int argc, char *argv[])
 
 		// Get the rank of the processor
 		MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
+		
 		// Get the name of the processor
 		MPI_Get_processor_name(processor_name, &name_len);
 		
@@ -35,15 +35,31 @@ int main (int argc, char *argv[])
 			}
 		}
 		if (size_found!=1) {
-			printf("The number of processes must be 1/2/3/4/6/12. \n");
+			if (world_rank == 0) {
+				printf("The number of processes must be 1/2/3/4/6/12. \n");
+			}
 			MPI_Finalize();
-			return -1;
+			return 0;
 		}
-		printf("process %d saninty passed\n", world_rank);
 		// ********** OPERATIONS START FROM HERE **********
 
 		int row_count_per_process = 1200/world_size;
 
+		// ********** IRecv Start First ****************
+		// ********** Overlap with both sending and generating, in addition, calculating **********
+		
+		// Prepare RequestList
+		requestList = (MPI_Request*) malloc(row_count_per_process * sizeof(MPI_Request));
+		// Prepare message buffer;
+		int msgbuf[row_count_per_process][1024];
+		
+		// Start Receiving
+		int k;
+		for (k = 0; k < row_count_per_process; k++) {
+			int row_num = world_rank * row_count_per_process + k;
+			MPI_Irecv(&msgbuf[k], 1024, MPI_INT, 0, row_num, MPI_COMM_WORLD, &(requestList[k]));
+			//printf("row %d received by rank %d\n", row_num, world_rank);
+		}		
 		// Once receiving channel is working, generating array while sending it.
 		if (world_rank==0) 
 		{
@@ -62,12 +78,12 @@ int main (int argc, char *argv[])
 			{
 				// fill row with random numbers within 0-99.
 				for (col=0; col<1024; col++) {
-					array[row][col] = rand() % 100;
+					array[row][col] = rand();
 				}
 				MPI_Isend(&array[row], 1024, MPI_INT, prank, row, MPI_COMM_WORLD, &requestNull);	
+				//printf("row %d sent to prank %d\n", row, prank);
 				row++;
-				if (row >= row_count_per_process) {
-					row = 0;
+				if (row % row_count_per_process == 0) {
 					prank++;
 				}
 				
@@ -75,51 +91,24 @@ int main (int argc, char *argv[])
 			
 		} 
 
-		printf("process %d sending passed\n", world_rank);
-		// ********** IRecv Start First ****************
-		// ********** Overlap with both sending and generating, in addition, calculating **********
-		
-		// Prepare RequestList
-		requestList = (MPI_Request*) malloc(row_count_per_process * sizeof(MPI_Request));
-		// Prepare msg;
-		int msg[row_count_per_process][1024];
-			
-		
-		// Start Receiving
-		int k;
-		for (k = 0; k < row_count_per_process; k++) {
-			int tag = world_rank * row_count_per_process + k;
-			MPI_Irecv(&msg[k], 1024, MPI_INT, 0, tag, MPI_COMM_WORLD, &(requestList[tag]));
-		}		
-		printf("process %d receiving passed\n", world_rank);
 		// ****** Calculating *******
 		int calculated = 0;
-		int x = 0;
-		while ( 1 == 1) 
+		while ( calculated < row_count_per_process) 
 		{
-			int flag = 0;
-			int mod = x % row_count_per_process;
-			int tag = world_rank * row_count_per_process + mod;
-			MPI_Test(&(requestList[tag]), &flag, &status);
-			printf("proc %d mpi_test passed\n", world_rank);
-			if (flag == 1) {
-				// Calculating Average
-				int sum = 0;
-				int i;
-				for (i = 0; i < 1024; i++) {
-					sum = sum + msg[tag][i];
-				}
-				double avg = (double)sum/1024;
-				printf("Process #%d (%s) : average of row #%d = %f\n", world_rank, processor_name, tag, avg);
-				calculated++;
+			int index;
+			MPI_Waitany(row_count_per_process, requestList, &index, &status);
+			// Calculating Average
+			int sum = 0;
+			int i;
+			for (i = 0; i < 1024; i++) {
+				sum = sum + msgbuf[index][i];
 			}
-			if (calculated >= row_count_per_process) {
-				break;
-			}
-			x++;
+			int row_num = world_rank * row_count_per_process + index;
+			double avg = (double)sum/1024;
+			printf("Process #%d (%s) : average of row #%d = %f\n", world_rank, processor_name, row_num, avg);
+			calculated++;
 		}
 		
-		printf("process %d calculating passed\n", world_rank);
 	
 		// Finalize the MPI environment
 		MPI_Finalize();

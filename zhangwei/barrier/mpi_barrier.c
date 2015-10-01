376dda11e7c0104b77b4661b405ef5f656a843bb
zhangwei217245 <zhangwei217245@gmail.com>
2015-10-01 14:35:02 -0500

    update

--- zhangwei/barrier/mpi_barrier.c
+++ zhangwei/barrier/mpi_barrier.c
@@ -14,7 +14,27 @@ char * getTimestamp(){
 
 
 int main(int argc, char *argv[])
-{
+{	
+
+		int world_size;
+		int world_rank;
+
+		char processor_name[MPI_MAX_PROCESSOR_NAME];
+		int name_len;
+
+		// **********  INITIALIZING + PROCESS INFO RETRIEVE ***********
+		// Initialize the MPI environment
+		MPI_Init(&argc, &argv);
+
+		// Get the number of processes
+		MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+		// Get the rank of the processor
+		MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+		// Get the name of the processor
+		MPI_Get_processor_name(processor_name, &name_len);
 		
+		printf("[%s]: Process #%d: passed barrier.", getTimestamp(), world_rank);
 
 }

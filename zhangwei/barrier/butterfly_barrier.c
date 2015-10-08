751c10b808ea3f2dd6e2de9a10d30cd4a4eaeada
zhangwei217245 <zhangwei217245@gmail.com>
2015-10-07 23:10:51 -0500

    update code

--- zhangwei/barrier/butterfly_barrier.c
+++ zhangwei/barrier/butterfly_barrier.c
@@ -4,6 +4,10 @@
 
 
 int my_barrier(MPI_Comm comm){
+		return butterfly_barrier_plain(comm);
+}
+
+int butterfly_barrier(MPI_Comm comm) {
 		int world_size, world_rank;
 		int flag = 1, i = 0, x = 0;
 		
@@ -39,5 +43,38 @@ int my_barrier(MPI_Comm comm){
 						break;
 				}
 		}
+		return 0;
+}
+
+int butterfly_barrier_plain(MPI_Comm comm){
+		int world_size, world_rank;
+		int i = 0, x = 0;
+		
+		MPI_Status status;
+		
+		// Get the number of processes
+		MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+		// Get the rank of the processor
+		MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);	
+
+		int last_mid = world_size;
+		
+		int totalSteps = (int)lg((double) world_size);
+		for (i = 1; i <= totalSteps; i++){
+				int pow_2 = (int)pow((double)2, (double)i);
+				int mid = world_size / pow_2;
+				int addr = 0;
+				if (world_rank % last_mid < mid) {
+						addr = world_rank + mid;
+				} else {
+						addr = world_rank - mid;
+				}
+				int x;
+				MPI_Send(0, 1, MPI_INT, addr, 0, MPI_COMM_WORLD);
+				MPI_Recv(&x, 1, MPI_INT, addr, 0, MPI_COMM_WORLD, &status);
+				last_mid = mid;
+		}
+		printf("[%s]: Butterfly Barrier done for #%d.\n", getTimeString(), world_rank);
 		return 0;
 }

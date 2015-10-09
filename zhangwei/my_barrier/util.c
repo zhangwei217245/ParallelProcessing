fcb544a8d3b59a2c3ac9349904c7d8e1a4a72aec
zhangwei217245 <zhangwei217245@gmail.com>
2015-10-08 22:54:11 -0500

    plain version of my_barrier using dissemination algorithm

---
+++ zhangwei/my_barrier/util.c
@@ -0,0 +1,34 @@
+#include "util.h"
+#include <math.h>
+
+int clk_gettime(int clk_id, struct timespec* t) {
+		return clock_gettime(clk_id, t);
+}
+double lg(double n){
+		return log(n)/log(2);
+}
+
+int ceiling(double n){
+		return (int)ceil(n);
+}
+double flooor(double n){
+		return floor(n);
+}
+char * getTimestamp(){
+		static char buff[20];
+		struct timespec tps;
+		clk_gettime(CLOCK_REALTIME, &tps);
+		unsigned long now = tps.tv_sec * 1000 + tps.tv_nsec / 1000000;
+		sprintf(buff, "%lu", now);
+		return buff;
+}
+char * getTimeString(){
+		static char buff[20];
+		struct timespec tps;
+		clk_gettime(CLOCK_REALTIME, &tps);
+		unsigned long now_milli = tps.tv_nsec / 1000000;
+		time_t now = tps.tv_sec;
+		strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+		sprintf(buff, "%s:%03lu", buff, now_milli);
+		return buff;
+}
---
+++ zhangwei/my_barrier/util.h
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int clk_gettime(int clk_id, struct timespec* t);
+double lg(double n);
+double flooor(double n);
+int ceiling(double n);
+char * getTimestamp();
+char * getTimeString();
